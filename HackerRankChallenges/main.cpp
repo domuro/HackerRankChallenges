e086bf447a0d8d1cab870f3de9bc0ec444320d34
Derek Omuro <derek.omuro@gmail.com>
2014-08-16 14:44:32 -0700

    Initial Commit

---
+++ HackerRankChallenges/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HackerRankChallenges
+//
+//  Created by Derek Omuro on 8/16/14.
+//  Copyright (c) 2014 domuro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
